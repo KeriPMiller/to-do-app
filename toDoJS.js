9790f9307ef5b8642c022c7ff231457a7d9e6009
KeriPMiller <KeriPMiller@users.noreply.github.com>
2017-07-27 07:36:33 -0400

    add feature added

--- toDoJS.js
+++ toDoJS.js
@@ -1,3 +1,14 @@
 console.log('toDoJS.js is connected');
 
-var toDoList = document.getElementById('toDoList');
+function newTask() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("taskInput").value;
+  var task = document.createTextNode(inputValue);
+  li.appendChild(task);
+
+  if ( inputValue === '' ) {
+    alert("Task is empty");
+  } else {
+    document.getElementById("toDoList").appendChild(li);
+  }
+}
