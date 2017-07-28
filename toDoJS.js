38fb8bd2b852d3d1b6866dd9723919f32c33f25a
KeriPMiller <KeriPMiller@users.noreply.github.com>
2017-07-28 08:38:22 -0400

    formatting

--- toDoJS.js
+++ toDoJS.js
@@ -7,7 +7,7 @@ var i;
 for (i = 0; i < myNodelist.length; i++) {
   var span = document.createElement("SPAN");
   // makes neat x
-  var txt = document.createTextNode("\u00D7");
+  var txt = document.createTextNode(" X");
   span.className = "close";
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
